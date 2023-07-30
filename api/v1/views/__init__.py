a64f200352c576945375427c3848c258bf786027
nazarenegena <genarene96@gmail.com>
2023-07-30 14:29:33 +0000

    fixed bug

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,5 +4,7 @@ from flask import Blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import
+from api.v1.views.users import *
+from api.v1.views.places_reviews import
 
 app_views = Blueprint('airbnb', __name__, url_prefix='/api/v1')
